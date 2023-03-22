33ea564db1b6452b800a7eafd08c634b7a8817a1
jasonleehodges <jason.hodges@domo.com>
2023-03-22 16:45:54 -0600

    STEP 04 - Fetch Data

--- app.js
+++ app.js
@@ -6,8 +6,31 @@
 //
 // For additional documentation on how you can query your data, please refer to
 // https://developer.domo.com/docs/dev-studio/dev-studio-data
-domo.get('/data/v1/returns?limit=100')
-    .then(function(returns){
-      console.log("returns", returns);
-    });
 
+async function loadData() {
+  const returns = await domo.get("/data/v1/returns?limit=100");
+
+  const returnsElement = document.querySelector("#returns");
+  returns.forEach((item, index) => {
+    const row = document.createElement("li");
+    row.setAttribute("class", "list-group-item"); // bootstrap class name
+    row.innerHTML = generateRow(item, index);
+    returnsElement.appendChild(row);
+  });
+}
+
+function generateRow(item, index) {
+  return `
+      <!-- Row of Return Data -->
+      <div class="itemContainer">
+          <div>${item.storeNumber}</div>
+          <div>${item.customerName}</div>
+          <div>${item.itemReturned}<div class="sku">#${item.SKU}</div></div>
+          <div>${item.reasonForReturn}</div>
+          <div>
+            <span class="badge badge-light">0</span>
+            <button class="btn btn-link">Add Comment</button>
+          </div>
+      </div>   
+     `;
+}
